a06cce187bd4a4e2b263b6449136736a019be53f
Chris M <habibdev96@gmail.com>
2021-02-10 06:08:51 -0700

    created navbar & hero section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const nav = document.querySelector(".custom-nav");
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("bg-dark");
+  } else {
+    nav.classList.remove("bg-dark");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
