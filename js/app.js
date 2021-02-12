a040d71bfc80b2f5bbc202f09a5f3ca728268dd5
Chris M <habibdev96@gmail.com>
2021-02-12 10:51:35 -0700

    created cards in talent section

--- js/app.js
+++ js/app.js
@@ -21,3 +21,42 @@ navLink.forEach((link) => {
     navbarCollapse.classList.remove("show");
   });
 });
+
+// show talent
+
+// Show an element
+var show = function (elem) {
+  elem.classList.add("is-visible");
+};
+
+// Hide an element
+var hide = function (elem) {
+  elem.classList.remove("is-visible");
+};
+
+// Toggle element visibility
+var toggle = function (elem) {
+  elem.classList.toggle("is-visible");
+};
+
+// Listen for click events
+document.addEventListener(
+  "click",
+  function (event) {
+    // Make sure clicked element is our toggle
+    if (!event.target.classList.contains("toggle")) return;
+
+    // Prevent default link behavior
+    event.preventDefault();
+
+    // Get the content
+    var content = document.querySelector(event.target.hash);
+    if (!content) return;
+
+    console.log(content);
+
+    // Toggle the content
+    toggle(content);
+  },
+  false
+);
